23de8b02576de0ebaea52348bb4a039ede637a87
sirawitch <sirawitchbu@kkumail.com>
2022-04-25 00:10:04 +0700

    done

---
+++ want_to_kick_app/android/app/src/main/kotlin/com/example/want_to_kick_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.want_to_kick_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ want_to_kick_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:want_to_kick_app/screens/login_screen.dart';
+import 'models/authentication.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider.value(value: Authentication())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          primarySwatch: Colors.blue,
+        ),
+        home: const LoginScreen(),
+      ),
+    );
+  }
+}
